fd4ac7c46320ffcb440b0a4df2af6e0daa22a840
Gary_Hou <52512824+hougarry@users.noreply.github.com>
2023-09-13 10:42:09 +0800

    Update HTML-notes.py

--- WebDesign/HTML-notes.py
+++ WebDesign/HTML-notes.py
@@ -2,3 +2,29 @@
 
 <link href='styles.css' rel='stylesheet' />  
 #<link> tag with rel='stylesheet', it's telling the browser that the linked resource (usually a .css file) contains styles that should be applied to the current HTML document,
+
+<form method="post" action='https://register-demo.freecodecamp.org'>
+#this HTML code represents an empty form that, when submitted, will send its data to the specified URL 'https://register-demo.freecodecamp.org'.
+#the method attribute specifies the HTTP method that will be used when submitting the form.
+
+label{
+  display: block;
+  margin:0.5rem 0;
+}
+#0.5rem: This is the top and bottom margin. It sets a margin of 0.5 "rem" units on the top and 0.5 "rem" units on the bottom of the element. The "rem" unit is relative to the font size of the root element (typically the <html> element).
+#0: This is the right and left margin. It sets a margin of 0 units on the right and 0 units on the left of the element.
+
+#common self-closed element in html:
+<area />: Defines an area inside an image map.
+<base />: Specifies a base URL for relative URLs in a document.
+<br />: Inserts a line break.
+<col />: Specifies column properties for a table column group.
+<embed />: Embeds external content like multimedia.
+<hr />: Creates a horizontal rule (line).
+<img />: Embeds an image.
+<input />: Creates an input field.
+<link />: Defines relationships between the current document and external resources like stylesheets.
+<meta />: Provides metadata about the HTML document.
+<source />: Specifies multiple media resources for media elements like <audio> and <video>.
+<track />: Specifies text tracks for media elements.
+<wbr />: Suggests an optional line break opportunity.
